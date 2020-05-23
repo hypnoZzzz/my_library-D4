9de578c8a46b8c89ac4060f18ce0e3666c8a48a6
hypnoZzzz <phin_crow@inbox.ru>
2020-05-23 16:20:53 +0300

    first commit

---
+++ common/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from common.models import UserProfile
+
+
+@admin.register(UserProfile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
--- my_site/urls.py
+++ my_site/urls.py
@@ -1,22 +1,7 @@
-"""my_site URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from p_library import views
 from p_library.views import AuthorEdit, AuthorList, author_create_many, books_authors_create_many
 
@@ -32,6 +17,7 @@ urlpatterns = [
     path('authors', AuthorList.as_view(), name='author_list'),
     path('author/create_many', author_create_many, name='author_create_many'),
     path('author_book/create_many', books_authors_create_many, name='author_book_create_many'),
+    path('', include('common.urls', namespace='common')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
